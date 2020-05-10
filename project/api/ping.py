bf5994c75e081957d028f4344a358d5c7126c4c2
Shaun B <me@shaunb.ca>
2020-05-09 21:16:59 -0700

    Part 1: Pytest Setup

---
+++ project/api/ping.py
@@ -0,0 +1,20 @@
+# project/api/ping.py
+
+
+from flask import Blueprint
+from flask_restx import Resource, Api
+
+
+ping_blueprint = Blueprint('ping', __name__)
+api = Api(ping_blueprint)
+
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(Ping, '/ping')
