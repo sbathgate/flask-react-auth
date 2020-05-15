321f589517a6b73d900048ddf0734024704cef46
Shaun B <me@shaunb.ca>
2020-05-14 18:18:59 -0700

    Part 2: Remaining Routes

--- project/api/users.py
+++ project/api/users.py
@@ -52,6 +52,32 @@ class Users(Resource):
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
 
+    @api.expect(user, validate=True)
+    def put(self, user_id):
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        user.username = username
+        user.email = email
+        db.session.commit()
+        response_object["message"] = f"{user.id} was updated!"
+        return response_object, 200
+
+    def delete(self, user_id):
+        response_object = {}
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        db.session.delete(user)
+        db.session.commit()
+        response_object["message"] = f"{user.email} was removed!"
+        return response_object, 200
+
 
 api.add_resource(UsersList, "/users")
 api.add_resource(Users, "/users/<int:user_id>")
--- project/tests/functional/test_users.py
+++ project/tests/functional/test_users.py
@@ -3,6 +3,8 @@
 
 import json
 
+import pytest
+
 from project.api.models import User
 
 
@@ -86,3 +88,72 @@ def test_all_users(test_app, test_database, add_user):
     assert "michael@mherman.org" in data[0]["email"]
     assert "fletcher" in data[1]["username"]
     assert "fletcher@notreal.com" in data[1]["email"]
+
+
+def test_remove_user(test_app, test_database, add_user):
+    test_database.session.query(User).delete()
+    user = add_user("user-to-be-removed", "remove-me@testdriven.io")
+    client = test_app.test_client()
+    resp_one = client.get("/users")
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert len(data) == 1
+    resp_two = client.delete(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "remove-me@testdriven.io was removed!" in data["message"]
+    resp_three = client.get("/users")
+    data = json.loads(resp_three.data.decode())
+    assert resp_three.status_code == 200
+    assert len(data) == 0
+
+
+def test_remove_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.delete("/users/999")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert "User 999 does not exist" in data["message"]
+
+
+def test_update_user(test_app, test_database, add_user):
+    user = add_user("user-to-be-updated", "update-me@testdriven.io")
+    client = test_app.test_client()
+    resp_one = client.put(
+        f"/users/{user.id}",
+        data=json.dumps({"username": "me", "email": "me@testdriven.io"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert f"{user.id} was updated!" in data["message"]
+    resp_two = client.get(f"/users/{user.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "me" in data["username"]
+    assert "me@testdriven.io" in data["email"]
+
+
+@pytest.mark.parametrize(
+    "user_id, payload, status_code, message",
+    [
+        [1, {}, 400, "Input payload validation failed"],
+        [1, {"email": "me@testdriven.io"}, 400, "Input payload validation failed"],
+        [
+            999,
+            {"username": "me", "email": "me@testdriven.io"},
+            404,
+            "User 999 does not exist",
+        ],
+    ],
+)
+def test_update_user_invalid(
+    test_app, test_database, user_id, payload, status_code, message
+):
+    client = test_app.test_client()
+    resp = client.put(
+        f"/users/{user_id}", data=json.dumps(payload), content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == status_code
+    assert message in data["message"]
