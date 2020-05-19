3f3b9ac1c20e7127d7e6f3c59050518c33019b3e
Shaun B <me@shaunb.ca>
2020-05-19 12:09:09 -0700

    Part 1: React Setup - UsersList Prop

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -6,10 +6,12 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 # instantiate the extensions
 db = SQLAlchemy()
+cors = CORS()
 admin = Admin(template_mode="bootstrap3")
 
 
@@ -24,7 +26,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
-
+    cors.init_app(app, resources={r"*": {"origins": "*"}})
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
 
